9be71d88c2d5d9d03f5fbc566898b09ac2012826
capJavert <ante.baric3@gmail.com>
2015-11-22 06:28:05 +0100

    .

--- backend/modules/game/controllers/IndexController.php
+++ backend/modules/game/controllers/IndexController.php
@@ -168,34 +168,26 @@ class IndexController extends Controller
         return 'nista';
     }
 
+    public function actionStart()
+    {
+        header('Content-type:application/json;charset=utf-8');
+        if(isset($_POST['id'])) {
+            $users = GameUser::find()->where(['user_id'=>$_POST['id']])->one();
+
+            foreach($users as $u) {
+                $game = Game::find()->where(['id'=>$u->game_id])->one();
+                if($game->live) return 'LIVE';
+            }
+
+            return 'NOT_LIVE';
+        }
+
+        return 'nista';
+    }
+
     public function actionMessagesget()
     {
         header('Content-type:application/json;charset=utf-8');
-        //if(isset($_POST['game']) && isset($_POST['id'])) {
-        /*$user = GameUser::find()->where(['game_id'=>1, 'user_id'=>6])->one();
-        $messages = Message::find()->where(['team'=>$user->team])->all();
-        $data = [];
-
-        $i= 0; foreach($messages as $m) {
-        $ids = $m->ids;
-        $ids = explode(';', $ids);
-
-        $skip = false;
-        foreach($ids as $id) {
-            if(!$id) break;
-            if($id == 6) $skip = true;
-            echo $skip;
-        }
-        if(!$skip)$m->ids = $m->ids.'6;';
-        $m->save();
-
-        if($skip) break;
-        else $data[$i] = $m->text;
-        $i++; }
-
-        return json_encode($data);
-        //}*/
-
         if(isset($_POST['game']) && isset($_POST['id'])) {
             $user = GameUser::find()->where(['game_id'=>$_POST['game'], 'user_id'=>$_POST['id']])->one();
             $messages = Message::find()->where(['team'=>$user->team])->all();
