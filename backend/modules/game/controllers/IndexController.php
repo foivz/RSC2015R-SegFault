3cb9c0c0100d2461d815dcc1eaf02d776c551f91
capJavert <ante.baric3@gmail.com>
2015-11-22 11:09:43 +0100

    .

--- backend/modules/game/controllers/IndexController.php
+++ backend/modules/game/controllers/IndexController.php
@@ -181,6 +181,8 @@ class IndexController extends Controller
         if(isset($_POST['id'])) {
             $users = GameUser::find()->where(['user_id'=>$_POST['id']])->all();
 
+            return $_POST['id'];
+
             foreach($users as $u) {
                 $game = Game::findOne($u->game_id);
                 if($game->live) return $game->id;
